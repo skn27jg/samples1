b8d4c9c6ad84de6a39fd011715c12cd3c2c14ed1
skn27jg <skn27.jg@gmail.com>
2026-02-23 12:23:06 +0900

    화면개발완료

--- page/main.py
+++ page/main.py
@@ -15,3 +15,15 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
